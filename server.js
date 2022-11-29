cee6c41a0c0f3f6805de0b5829d2c6a440731491
Diksha Sharma <diksha@softobiz.com>
2022-11-29 14:19:37 +0530

    "2.19"

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express=require('express')
 const app =express();
 const {connectMongoose,User}=require("./database.js")
 const ejs =require('ejs');
+const passport =require('passport');
+const { initializingPassport } = require('./passportConfig.js');
+const expressSession =require("express-session");
 
 connectMongoose(); 
+initializingPassport(passport);
 
 app.get("/",(req,res)=>{
     res.render("index");
@@ -19,6 +23,13 @@ app.get('/login',(req,res)=>{
 app.use(express.json());
 
 app.use(express.urlencoded({extended:true}));
+app.use(expressSession({secret:"secret",resave:false,saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 
 app.set('view engine','ejs');
 
@@ -32,6 +43,11 @@ const newUser= await User.create(req.body);
 
 res.status(201).send(newUser)
 });
+
+app.post('/login',passport.authenticate('local',{failureRedirect:'/register',successRedirectL:'/'}),async(req,res)=>{
+
+});
+
 
 app.listen(3000,()=>{
     console.log("server start")
